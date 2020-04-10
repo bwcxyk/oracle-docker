f51c75309df693e421bbdb4128b449673e9b49b6
不忘初心 <1624717079@qq.com>
2020-04-10 12:44:50 +0800

    修改基础镜像为centos

--- 12.2.0.1/installDBBinaries.sh
+++ 12.2.0.1/installDBBinaries.sh
@@ -39,32 +39,5 @@ cd $INSTALL_DIR       && \
 unzip $INSTALL_FILE_1 && \
 rm $INSTALL_FILE_1    && \
 $INSTALL_DIR/database/runInstaller -silent -force -waitforcompletion -responsefile $INSTALL_DIR/$INSTALL_RSP -ignoresysprereqs -ignoreprereq && \
-cd $HOME
-
-# Remove not needed components
-# APEX
-rm -rf $ORACLE_HOME/apex && \
-# ORDS
-rm -rf $ORACLE_HOME/ords && \
-# SQL Developer
-rm -rf $ORACLE_HOME/sqldeveloper && \
-# UCP connection pool
-rm -rf $ORACLE_HOME/ucp && \
-# All installer files
-rm -rf $ORACLE_HOME/lib/*.zip && \
-# OUI backup
-rm -rf $ORACLE_HOME/inventory/backup/* && \
-# Network tools help
-rm -rf $ORACLE_HOME/network/tools/help && \
-# Database upgrade assistant
-rm -rf $ORACLE_HOME/assistants/dbua && \
-# Database migration assistant
-rm -rf $ORACLE_HOME/dmu && \
-# Remove pilot workflow installer
-rm -rf $ORACLE_HOME/install/pilot && \
-# Support tools
-rm -rf $ORACLE_HOME/suptools && \
-# Temp location
-rm -rf /tmp/* && \
 # Database files directory
 rm -rf $INSTALL_DIR/database
