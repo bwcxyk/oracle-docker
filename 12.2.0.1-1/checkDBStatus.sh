c1fa7965688677797222f69d3be60cfa4382fe35
不忘初心 <1624717079@qq.com>
2020-04-13 17:05:58 +0800

    修复数据库安装检查错误bug

--- 12.2.0.1-1/checkDBStatus.sh
+++ 12.2.0.1-1/checkDBStatus.sh
@@ -6,14 +6,11 @@
 # Since: May, 2017
 # Author: gerald.venzl@oracle.com
 # Description: Checks the status of Oracle Database.
-# Return codes: 0 = PDB is open and ready to use
-#               1 = PDB is not open
-#               2 = Sql Plus execution failed
+
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 # 
 
 ORACLE_SID="`grep $ORACLE_HOME /etc/oratab | cut -d: -f1`"
-OPEN_MODE="READ WRITE"
 ORAENV_ASK=NO
 source oraenv
 
@@ -21,15 +18,14 @@ source oraenv
 status=`sqlplus -s / as sysdba << EOF
    set heading off;
    set pagesize 0;
-   SELECT DISTINCT open_mode FROM v\\$pdbs WHERE open_mode = '$OPEN_MODE';
    exit;
 EOF`
 
 # Store return code from SQL*Plus
 ret=$?
 
-# SQL Plus execution was successful and PDB is open
-if [ $ret -eq 0 ] && [ "$status" = "$OPEN_MODE" ]; then
+# SQL Plus execution was successful
+if [ $ret -eq 0 ] ; then
    exit 0;
 # SQL Plus execution failed
 else
